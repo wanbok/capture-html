735662c0ce91d1fc677a81c9cf898d3d54ea4f18
Wanbok Choi <choi@wanbok.com>
2013-10-06 00:23:38 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaptureHtml::Application.config.secret_key_base = '4605552d51288868af3fc991d42c148bfa7e56e632d303e411dd2d767420079f70447217b539d5b247483a68a37437ecfeaf98b6499e55191ca45d59f10f1b66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CaptureHtml::Application.config.session_store :cookie_store, key: '_capture_html_session'
