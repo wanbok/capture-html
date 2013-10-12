2646db6bd9d973d5783768270cb2d63ab2825bde
Wanbok Choi <choi@wanbok.com>
2013-10-12 23:10:15 +0900

    Complete to show prototype

--- app/controllers/capture_controller.rb
+++ app/controllers/capture_controller.rb
@@ -1,19 +1,27 @@
 class CaptureController < ApplicationController
-  protect_from_forgery except: :craete
+  protect_from_forgery except: :create
 
   def create
-    if params[:url]
-      kit = capture(params)
-      img = Magick::Image::from_blob(kit.to_jpg).first
+    if params[:product]# && params[:html]
+      Dir.mkdir 'files' unless Dir.exist? 'files'
+      image_name = 'files/' + params[:product] + '.jpg'
+      unless File.exist? image_name
+        kit = capture(params)
+        img = Magick::Image::from_blob(kit.to_jpg).first
 
-      sigma = params[:sigma] || "20.0"
-      send_data img.blur_image(0.0, sigma.to_f).to_blob,
-        filename: 'image.jpg',
-        type: 'image/jpeg',
-        disposition: 'inline'
+        sigma = params[:sigma] || "20.0"
+        img.blur_image(0.0, sigma.to_f).write image_name
+      end
+
+      File.open(image_name, 'rb') do |f|
+        send_data f.read,
+          filename: image_name,
+          type: 'image/jpeg',
+          disposition: 'inline'
+      end
     else
       msg = {
-        message: "url is required."
+        message: "product and html is required."
       }
       render json: msg, status: :bad_request
     end
@@ -24,6 +32,7 @@ class CaptureController < ApplicationController
     width = params[:width] || 0
     height = params[:height] || 0
     quality = params[:quality] || 30
-    IMGKit.new(params[:url], width: width, height: height, quality: quality)
+    # IMGKit.new(params[:html], width: width, height: height, quality: quality)
+    IMGKit.new(File.new('files/115310.html'), width: width, height: height, quality: quality)
   end
 end
